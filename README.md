a86f6182816944525a1b36c8f28c041639a7ceb8
lucoiso <lucas.oliveiravb@gmail.com>
2024-02-08 22:35:54 +0000

    conan package manager

--- README.md
+++ README.md
@@ -4,17 +4,7 @@
 
 # Dependencies
 
-1. Vcpkg (w/ ENVIRONMENT VARIABLE VCPKG_ROOT set to the vcpkg root directory)
-2. CMake v3.28 (Officially supporting C++20 Modules)
-3. Vulkan SDK v1.3.261 (Does not need any modules except the SDK core)
-4. Git
-
-# Setup
-
-- Run these commands:
-
-```
-git clone https://github.com/lucoiso/renderer-application.git
-cd renderer-application
-git submodule update --init --recursive
-```
+1. C++20
+2. Python w/ conan and ninja
+3. CMake 3.28
+4. Vulkan SDK v1.3.268 (Does not need any modules except the SDK core)
---
+++ conanfile.py
@@ -0,0 +1,55 @@
+# Copyright Notices: [...]
+
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class RendererApplicationRecipe(ConanFile):
+    name = "renderer-application"
+    version = "0.0.1"
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        # https://conan.io/center/recipes/glfw
+        self.requires("glfw/[>=3.3]")
+
+        # https://conan.io/center/recipes/imgui
+        self.requires("imgui/1.90.1-docking")
+
+        # https://conan.io/center/recipes/boost
+        self.requires("boost/[>=1.83]")
+
+        # https://conan.io/center/recipes/benchmark
+        self.requires("benchmark/[>=1.8]")
+
+        # https://conan.io/center/recipes/catch2
+        self.requires("catch2/[>=3.4]")
+
+        # https://conan.io/center/recipes/glm
+        self.requires("glm/[>=0.9]")
+
+        # https://conan.io/center/recipes/tinygltf
+        self.requires("tinygltf/[>=2.8]")
+
+        # https://conan.io/center/recipes/volk
+        self.requires("volk/[>=1.3]")
+
+        # https://conan.io/center/recipes/vulkan-memory-allocator
+        self.requires("vulkan-memory-allocator/cci.20231120")
+
+        # https://conan.io/center/recipes/vulkan-headers
+        self.requires("vulkan-headers/[>=1.3]", override=True)
+
+        # https://conan.io/center/recipes/spirv-tools
+        self.requires("spirv-tools/[>=1.3]")
+
+        # https://conan.io/center/recipes/glslang
+        # self.requires("glslang/[>=11.7]")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.28]")
+
+    def layout(self):
+        cmake_layout(self)
